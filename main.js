01be55e8c3109264c0054cee4c33f27db86661e9
Bruno <yatosad@gmail.com>
2023-10-04 23:43:59 -0300

    Ajustes

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
+$(document).ready(function(){
+    $('#numero-contato').mask("(00) 00000-0000", 
+    {placeholder: "(00) 00000-0000"})
+})
+
 const form = document.getElementById('form-tel')
 const numerosCont = [];
+const nomesCont = [];
 
 let linhas = ' ';
 
@@ -12,12 +18,13 @@ form.addEventListener('submit', function(e){ //função para prevenir que o form
 })
 
 function adicionaLinha (){ //função para adicionar linhas na tabela com as informações novas
-    const inputNomeContato = document.getElementById('nome-contato') //variavel pra notas e nomes
+    
+    const inputNomeContato = document.getElementById('nome-contato') //variavel para o nome enumero
     const inputNumeroContato = document.getElementById('numero-contato')
 
-    numerosCont.push(parseFloat(inputNumeroContato.value));
+    numerosCont.push(parseFloat(inputNumeroContato.value)); //pegará o número dos contatos e transformará de string para float
 
-    let linha = '<tr>'
+    let linha = '<tr>' //adicionará ou substuirá as linhas da tabela que fizemos no html
     linha += `<td>${inputNomeContato.value}</td>`
     linha += `<td>${inputNumeroContato.value}</td>`
     linha += `</tr>`
@@ -25,16 +32,18 @@ function adicionaLinha (){ //função para adicionar linhas na tabela com as inf
     linhas += linha;
 
     inputNomeContato.value = '';
-    inputNumeroContato.value = ' ';
- 
-}
-
+    inputNumeroContato.value = '';
+        
+ }
 
 function atualizaLinha (){ //função para substituir e adicionar as linhas da tabela com as informações da função anterior
-    const atualizaAgenda = document.querySelector('tbody');
-    atualizaAgenda.innerHTML = linhas;
+    
+    const atualizaAgenda = document.querySelector('tbody'); //variável que pegara o corpo da tabela
+    atualizaAgenda.innerHTML = linhas; //innerhtml substuirá o texto contido no corpo da tabela
 }
 
 function atualizaNum (){ //função para dizer a quantidade de contatos na agenda
-    document.getElementById('numerototal').innerHTML = numerosCont.length
+    
+    document.getElementById('numerototal').innerHTML = numerosCont.length; // atualizará a quantidade de número no pé da tabela
 }
+
